10c37104cdc8e627126974cdb39df2073e003769
KeWang1017 <ke.wang@helloleap.ai>
2025-02-10 22:01:42 +0000

    change the projection back to default

--- lerobot/common/policies/pi0/configuration_pi0.py
+++ lerobot/common/policies/pi0/configuration_pi0.py
@@ -47,7 +47,7 @@ class PI0Config(PreTrainedConfig):
     tokenizer_max_length: int = 48
 
     # Projector
-    proj_width: int = 512
+    proj_width: int = 1024
 
     # Decoding
     num_steps: int = 10
