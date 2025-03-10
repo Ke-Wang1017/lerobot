de2b9c9992fb4df6e031d78a20a5ddc4f6775ed6
Ke-Wang1017 <superwk1017@gmail.com>
2025-03-10 15:40:53 +0000

    Update policy factory to support DiTFlow policy

--- lerobot/common/policies/factory.py
+++ lerobot/common/policies/factory.py
@@ -25,8 +25,8 @@ from lerobot.common.envs.configs import EnvConfig
 from lerobot.common.envs.utils import env_to_policy_features
 from lerobot.common.policies.act.configuration_act import ACTConfig
 from lerobot.common.policies.diffusion.configuration_diffusion import DiffusionConfig
-from lerobot.common.policies.pi0.configuration_pi0 import PI0Config
 from lerobot.common.policies.dit_flow.configuration_dit_flow import DiTFlowConfig
+from lerobot.common.policies.pi0.configuration_pi0 import PI0Config
 from lerobot.common.policies.pretrained import PreTrainedPolicy
 from lerobot.common.policies.tdmpc.configuration_tdmpc import TDMPCConfig
 from lerobot.common.policies.vqbet.configuration_vqbet import VQBeTConfig
@@ -44,6 +44,10 @@ def get_policy_class(name: str) -> PreTrainedPolicy:
         from lerobot.common.policies.diffusion.modeling_diffusion import DiffusionPolicy
 
         return DiffusionPolicy
+    elif name == "ditflow":
+        from lerobot.common.policies.dit_flow.modeling_dit_flow import DiTFlowPolicy
+
+        return DiTFlowPolicy
     elif name == "act":
         from lerobot.common.policies.act.modeling_act import ACTPolicy
 
@@ -65,10 +69,10 @@ def make_policy_config(policy_type: str, **kwargs) -> PreTrainedConfig:
         return TDMPCConfig(**kwargs)
     elif policy_type == "diffusion":
         return DiffusionConfig(**kwargs)
-    elif policy_type == "act":
-        return ACTConfig(**kwargs)
     elif policy_type == "ditflow":
         return DiTFlowConfig(**kwargs)
+    elif policy_type == "act":
+        return ACTConfig(**kwargs)
     elif policy_type == "vqbet":
         return VQBeTConfig(**kwargs)
     elif policy_type == "pi0":
